187352f16a824ad7e2463590754ba5ccf462275c
kimyoda <rladygks1210@naver.com>
2024-05-29 03:25:54 +0900

    [feat] 11_01_TypeScript React Setting

--- section11/src/index.tsx
+++ section11/src/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// 1. 타입선언  npm i @types/node @types/react @types/react-dom @types/jest 로다운
+// 2. tsconfig.json 설정
+// 3. 모든 JS -> JSX -> TSX로 변환
+// 4. 개별들을 하나씩 TSX로 에러를 잡으면서 변경
+
 const root = ReactDOM.createRoot(
   // 초기세팅
   document.getElementById("root") as HTMLElement
